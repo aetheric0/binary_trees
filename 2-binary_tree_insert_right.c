93b04243fb78698c86643aaa2bf68e99c31401dc
Monarch <monarchofaether@gmail.com>
2024-06-04 12:45:16 +0300

    newnode->left set to NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	newnode->n = value;
 	newnode->right = parent->right;
+	newnode->left = NULL;
 	newnode->parent = parent;
 
 	if (parent->right != NULL)
