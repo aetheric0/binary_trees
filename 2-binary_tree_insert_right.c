79abecba69fa5931c310f966e66ad856a502c247
Monarch <monarchofaether@gmail.com>
2024-06-04 12:43:43 +0300

    function to insert right node to a parent

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a right node to the parent
+ * @parent: parent node
+ * @value: value of new right node
+ * Return: pointer to new right node
+ **/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->right = parent->right;
+	newnode->parent = parent;
+
+	if (parent->right != NULL)
+		parent->right->parent = newnode;
+	parent->right = newnode;
+
+	return (newnode);
+}
