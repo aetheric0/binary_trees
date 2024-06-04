164e4137da0d97bada1c359d72545cddb1f9f441
Monarch <monarchofaether@gmail.com>
2024-06-04 13:24:54 +0300

    using recursive method coz its waaaaay better

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,44 +7,9 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *temp, *holder;
-
 	if (tree == NULL)
-	{
 		return;
-	}
-	temp = tree->left->left;
-	while (temp != NULL)
-	{
-		holder = temp->left;
-		free(temp);
-		temp = holder;
-	}
-	temp = tree->left->right;
-	while (temp != NULL)
-	{
-		holder = temp->right;
-		free(temp);
-		temp = holder;
-	}
-	free(tree->left);
-
-	temp = tree->right->left;
-	while (temp != NULL)
-	{
-		holder = temp->left;
-		free(temp);
-		temp = holder;
-	}
-
-	temp = tree->right->right;
-	while (temp != NULL)
-	{
-		holder = temp->right;
-		free(temp);
-		temp = holder;
-	}
-	free(tree->right);
-
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
