fbad18eb275bbb7854acab8ba9ff2c2ea23d8f82
Monarch <monarchofaether@gmail.com>
2024-06-05 18:06:19 +0300

    more conditions being considered

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,19 +10,17 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	binary_tree_t *first_holder, *second_holder;
+	binary_tree_t *first_holder;/* *second_holder; */
 
 	first_holder = (binary_tree_t *)first;
-	second_holder = (binary_tree_t *)second;
+/*	second_holder = (binary_tree_t *)second; */
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 	if (first == second)
 		return (first_holder);
-	if (second->parent == first)
+	if (first == first->parent || second == second->parent)
 		return (first_holder);
-	if (first->parent == second)
-		return (second_holder);
 	if (first->parent != NULL && second->parent != NULL)
 	{
 		if (first->parent == second->parent)
