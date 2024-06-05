3df99953d76bbffdf602a5cb9b2bb93db2d3e7be
Monarch <monarchofaether@gmail.com>
2024-06-05 17:51:39 +0300

    Finding Ancestors

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second node
+ * Return: pointer to Ancestor, NULL otherwise
+ **/
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *first_holder, *second_holder;
+
+	first_holder = (binary_tree_t *)first;
+	second_holder = (binary_tree_t *)second;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first->left == second || first->right == second)
+		return (first_holder);
+	if (second->left == first || second->right == second)
+		return (second_holder);
+	if (first->parent != NULL && second->parent != NULL)
+	{
+		if (first->parent == second->parent)
+			return (first->parent);
+		if (first->parent->parent != NULL &&
+		    second->parent->parent != NULL)
+		{
+			if (first->parent->parent == second->parent->parent)
+				return (first->parent->parent);
+			if (first->parent == second->parent->parent)
+				return (second->parent->parent);
+			if (second->parent == first->parent->parent)
+				return (first->parent->parent);
+		}
+	}
+	return (NULL);
+}
