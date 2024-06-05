e1dcbd2212b42e42243e02bfe982b9ccdb9024a9
Monarch <monarchofaether@gmail.com>
2024-06-05 13:52:38 +0300

    full binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,22 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_factor, right_factor;
-
 	if (tree == NULL)
 		return (0);
-	left_factor = (tree->left) ? 1 + binary_tree_balance(tree->left) : 0;
-	right_factor = (tree->right) ? 1 + binary_tree_balance(tree->right) : 0;
-	return (left_factor - right_factor);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t left_height = 0, right_height = 0;
+
+        if (tree == NULL)
+                return (0);
+        if (tree->left == NULL && tree->right == NULL)
+                return (0);
+        left_height = binary_tree_height(tree->left);
+        right_height = binary_tree_height(tree->right);
+
+        return ((left_height > right_height) ?
+                left_height + 1 : right_height + 1);
 }
